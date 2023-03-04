90d8d1be2b9fcf0c53bc1608f4c693dfd81372a1
rencewang <47406212+rencewang@users.noreply.github.com>
2023-03-04 17:01:42 -0500

    initial data cleaning

--- data/fang_data_processing.R
+++ data/fang_data_processing.R
@@ -6,3 +6,25 @@
 # Tasks: clean the data scraped from fang.com
 # a) 
 #-----------------------------------------------------------------------#
+
+rm(list = ls())
+setwd("/Volumes/Personal/GitHub/CSEC-491/data")
+library(haven)
+library(foreign)
+library(dplyr)
+library(purrr)
+library(readxl)
+
+
+# Clean all_communities_china data to select for city
+all_communities_china <- read_excel("all_communities_china.xlsx")
+all_communities_china_relevant <- subset(all_communities_china, select = -c(province, management_fee, parking, floor_ratio, greenery_rate, developer, management, school, info))
+wh_communities <- all_communities_china_relevant[all_communities_china_relevant$city == '武汉市',]
+wuchang_communities <- wh_communities[wh_communities$district == '武昌',]
+
+# Clean secondhand house data
+wh_secondhand <- read.csv("whfang_secondhandhouse_march.csv")
+
+
+# Clean new house data
+wh_newhouse <- read.csv("whfang_newhouse_march.csv")
