b7ddb23ede600955713b96a9b7e4b7bc5e76260a
rencewang <47406212+rencewang@users.noreply.github.com>
2023-03-04 15:36:08 -0500

    initial scraped data

---
+++ fang_data_processing.R
@@ -0,0 +1,8 @@
+#-----------------------------------------------------------------------#
+# Project: CSEC 491, Property Taxes in China
+# Date: 03/04/2023
+# Author: Lawrence Wang
+# #
+# Tasks: clean the data scraped from fang.com
+# a) 
+#-----------------------------------------------------------------------#
