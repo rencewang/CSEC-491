366ae268a3f403bc2955a14f6870ae7c79a0c09d
rencewang <47406212+rencewang@users.noreply.github.com>
2023-04-15 13:39:20 -0400

    new files

--- script.js
+++ script.js
@@ -1,23 +1,92 @@
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7/+esm';
+// import 'coordtransform';
+
 let map;
+let geourl = 'wuchang.geojson';
+let all_communities_url = 'wc_communities_imputed.csv';
 
 async function initMap() {
   //@ts-ignore
   const { Map } = await google.maps.importLibrary('maps');
+  const infoWindow = new google.maps.InfoWindow();
 
-  // Set the bounds of the map to the New York City area
+  function addMarkersToMap(latitudes, longitudes, names, area, units, price) {
+    // Loop through the arrays and add a marker for each location
+    for (let i = 0; i < latitudes.length; i++) {
+      // Convert the coordinates to WGS84
+
+      const marker = new google.maps.Marker({
+        position: { lat: latitudes[i], lng: longitudes[i] },
+        map: map,
+        title: names[i],
+      });
+
+      // Add an event listener to the marker to show the info window when clicked
+      marker.addListener('click', () => {
+        infoWindow.setContent(
+          names[i] +
+            ', area: ' +
+            area[i] +
+            ', units: ' +
+            units[i] +
+            ', price: ' +
+            price[i]
+        );
+        infoWindow.open(map, marker);
+      });
+    }
+  }
+
+  // Add all community markers to map
+  fetch('wc_communities_imputed.csv')
+    .then((response) => response.text())
+    .then((data) => {
+      const parsed_csv = d3.csvParse(data);
+      console.log(parsed_csv);
+
+      const latitudes = parsed_csv.map((d) => Number(d.latitude_gps));
+      const longitudes = parsed_csv.map((d) => Number(d.longitude_gps));
+      const names = parsed_csv.map((d) => d.community);
+      const area = parsed_csv.map((d) => d.avg_area);
+      const units = parsed_csv.map((d) => d.total_units);
+      const price = parsed_csv.map((d) => d.unit_price);
+
+      addMarkersToMap(latitudes, longitudes, names, area, units, price);
+    });
+
+  // Add the boundary of Wuchang to the map
+  fetch('wuchang.geojson')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const boundary = new google.maps.Data();
+      boundary.addGeoJson(data);
+      boundary.setStyle({
+        fillColor: 'transparent',
+        strokeColor: 'white',
+        strokeWeight: 3,
+      });
+      boundary.setMap(map);
+    });
+
+  // Set the bounds of the map to the Wuchang area
   var bounds = new google.maps.LatLngBounds(
     new google.maps.LatLng(30.25, 114.0),
     new google.maps.LatLng(30.85, 114.66)
   );
 
-  map = new Map(document.getElementById('map'), {
+  // Determine options for the map
+  var mapOptions = {
     center: { lat: 30.553, lng: 114.314 },
     zoom: 13,
     restriction: {
       latLngBounds: bounds,
       strictBounds: true,
     },
-  });
+    mapTypeId: 'satellite',
+  };
+
+  map = new Map(document.getElementById('map'), mapOptions);
 }
 
 initMap();
